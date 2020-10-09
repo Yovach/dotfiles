45e8b0d9516389b61da8ec26042e747fb75608b5
Quentin <31007601+Yovach@users.noreply.github.com>
2020-10-09 21:06:44 +0200

    Update and rename install.ps1 to install-main.ps1

---
+++ install-main.ps1
@@ -0,0 +1,14 @@
+winget install Microsoft.VisualStudioCode  ; 
+winget install Google.Chrome  ; 
+winget install Discord.Discord  ; 
+winget install Bitwarden.Bitwarden  ; 
+winget install Telegram.TelegramDesktop  ; 
+winget install Notepads.Notepads  ; 
+winget install Nvidia.GeForceExperience  ; 
+winget install voidtools.EverythingLite  ; 
+winget install Microsoft.PowerShell  ; 
+winget install ProtonVPN.ProtonVPN  ; 
+winget install Microsoft.Edge  ; 
+winget install Docker.DockerDesktop  ; 
+winget install Debian.Debian  ; 
+winget install Insomnia.Insomnia
