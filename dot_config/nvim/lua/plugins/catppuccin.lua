c014488178345f88267dda0ac9d5ec310f04f6c5
Quentin <contact@yovach.dev>
2024-01-10 12:06:36 +0100

    config nvim from scratch

---
+++ dot_config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,21 @@
+return {
+    "catppuccin/nvim",
+    config = function()
+        require('catppuccin').setup({
+            flavour = "mocha",
+
+            integrations = {
+                cmp = true,
+                gitsigns = true,
+                nvimtree = true,
+                treesitter = true,
+                -- fidget = true,
+                mason = true,
+                telescope = {
+                    enabled = true
+                }
+            }
+        })
+        vim.cmd.colorscheme "catppuccin"
+    end
+}
