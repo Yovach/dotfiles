80ce53c9ba0c73eb0808c15005fff084b6c17650
Quentin <contact@yovach.dev>
2024-01-11 15:30:44 +0100

    use lazygit for nvim config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,20 +1,2 @@
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
--- vim.g.maplocalleader = ' '
-
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
-
-require('lazy').setup('plugins')
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
