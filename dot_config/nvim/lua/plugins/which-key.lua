3c0ea43fcedb228d04469864a1a6c50ebff7c83b
Quentin <contact@yovach.dev>
2024-01-11 19:14:59 +0100

    update whichkey config

--- dot_config/nvim/lua/plugins/which-key.lua
+++ dot_config/nvim/lua/plugins/which-key.lua
@@ -5,23 +5,5 @@ return {
     vim.o.timeout = true
     vim.o.timeoutlen = 300
   end,
-  opts = {},
-  config = function()
-    require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-    }
-    -- register which-key VISUAL mode
-    -- required for visual <leader>hs (hunk stage) to work
-    require('which-key').register({
-      ['<leader>'] = { name = 'VISUAL <leader>' },
-      ['<leader>h'] = { 'Git [H]unk' },
-    }, { mode = 'v' })
-  end,
+  config = true,
 }
