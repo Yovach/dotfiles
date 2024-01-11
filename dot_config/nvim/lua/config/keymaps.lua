5de1b8d5e039d541189a65dcecbbcb286228031f
Quentin <contact@yovach.dev>
2024-01-11 15:35:18 +0100

    disable arrow keys

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Disable Arrow keys
+vim.keymap.set("n", "<Up>", "<Nop>", { silent = true })
+vim.keymap.set("n", "<Down>", "<Nop>", { silent = true })
+vim.keymap.set("n", "<Left>", "<Nop>", { silent = true })
+vim.keymap.set("n", "<Right>", "<Nop>", { silent = true })
