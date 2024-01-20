b7789549e151e58f02ef830517fe0e659cab8fc5
Quentin <contact@yovach.dev>
2024-01-20 15:20:40 +0100

    add autotag for treesitter

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,34 @@
 return {
-  -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
+  {
+    'windwp/nvim-ts-autotag',
   },
-  build = ':TSUpdate',
-
-  config = function()
-    local configs = require 'nvim-treesitter.configs'
-
-    configs.setup {
-      ensure_installed = { 'javascript', 'typescript', 'css', 'html', 'php', 'rust', 'jsdoc', 'json', 'markdown', 'sql', 'yaml', 'tsx' },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    opts = {
+      mode = "cursor",
+      max_lines = 3
     }
-  end,
+  },
+  {
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+
+    config = function()
+      local configs = require 'nvim-treesitter.configs'
+
+      configs.setup {
+        ensure_installed = { 'javascript', 'typescript', 'css', 'html', 'php', 'rust', 'jsdoc', 'json', 'markdown', 'sql', 'yaml', 'tsx' },
+        sync_install = false,
+        autotag = {
+          enable = true,
+        },
+        highlight = { enable = true },
+        indent = { enable = true },
+      }
+    end,
+  }
 }
