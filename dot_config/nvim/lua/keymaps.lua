cfb8e19201b4e55d30ea9d9662725b46a16bbbec
Quentin <contact@yovach.dev>
2024-01-24 16:26:53 +0100

    add comment context, lualine using catppuccin theme, update keymaps

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -24,8 +24,33 @@ vim.keymap.set({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>', { desc = 'Escape and 
 vim.keymap.set('n', '<C-S>', '<CMD>w<CR>', { silent = true, desc = 'Save file' })
 
 -- THE F1 BUTTON IS SO FUCKING ANNOYING BRO
-vim.keymap.set("v", "<F1>", "", { silent=true })
-vim.keymap.set("i", "<F1>", "", { silent=true })
-vim.keymap.set("n", "<F1>", "", { silent=true })
+vim.keymap.set("v", "<F1>", "<Nop>", { silent = true })
+vim.keymap.set("i", "<F1>", "<Nop>", { silent = true })
+vim.keymap.set("n", "<F1>", "<Nop>", { silent = true })
 
-vim.keymap.set("n", "<C-u>", "<CMD>NvimTreeToggle<CR>")
+-- Resize window
+vim.keymap.set("n", "<S-Up>", "<CMD>resize +2<CR>", { desc = "Increase window height", silent = true })
+vim.keymap.set("n", "<S-Down>", "<CMD>resize -2<CR>", { desc = "Decrease window height", silent = true })
+vim.keymap.set("n", "<S-Left>", "<CMD>vertical resize -2<CR>", { desc = "Decrease window width", silent = true })
+vim.keymap.set("n", "<S-Right>", "<CMD>vertical resize +2<CR>", { desc = "Increase window width", silent = true })
+
+-- Others
+vim.keymap.set("n", "<leader>wd", "<C-W>c", { desc = "Delete window", silent = true })
+vim.keymap.set("n", "<leader>wy", "<C-W>s", { desc = "Split window below", silent = true })
+vim.keymap.set("n", "<leader>wx", "<C-W>v", { desc = "Split window right", silent = true })
+
+-- Tab
+vim.keymap.set("n", "<S-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Prev buffer", silent = true })
+vim.keymap.set("n", "<S-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next buffer", silent = true })
+vim.keymap.set("n", "<S-j>", "<cmd>BufferLineMovePrev<cr>", { desc = "Move tab to left", silent = true, noremap = true })
+vim.keymap.set("n", "<S-k>", "<cmd>BufferLineMoveNext<cr>", { desc = "Move tab to right", silent = true })
+
+-- Move lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line up", silent = true })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line down", silent = true })
+
+-- ENABLE FILE EXPLORER (MY VSCODE KEYMAP)
+vim.keymap.set("n", "<C-u>", "<CMD>NvimTreeToggle<CR>", { desc = "Toggle the NvimTree", silent = true })
+
+-- Git
+vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>', { silent = true, desc = 'Git' })
