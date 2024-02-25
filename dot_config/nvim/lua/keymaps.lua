bc99b9c7a379407d29618ab22da700c6da22cc06
Quentin <contact@yovach.dev>
2024-02-25 23:01:52 +0100

    feat(nvim): replace nvim tree keymap by <leader>e

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -6,12 +6,6 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
--- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
-
 -- Disable Arrow keys
 vim.keymap.set('n', '<Up>', '<Nop>', { silent = true })
 vim.keymap.set('n', '<Down>', '<Nop>', { silent = true })
@@ -49,6 +43,6 @@ vim.keymap.set("n", "<S-k>", "<cmd>BufferLineMoveNext<cr>", { desc = "Move tab t
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line up", silent = true })
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line down", silent = true })
 
--- ENABLE FILE EXPLORER (MY VSCODE KEYMAP)
-vim.keymap.set("n", "<C-u>", "<CMD>NvimTreeToggle<CR>", { desc = "Toggle the NvimTree", silent = true })
+-- ENABLE FILE EXPLORER
+vim.keymap.set("n", "<leader>e", "<CMD>NvimTreeToggle<CR>", { desc = "Toggle the NvimTree", silent = true })
 
