7dcba732b9c3ab0eeb819615918648c312e3dfc9
Quentin <contact@yovach.dev>
2024-04-25 22:19:16 +0200

    feat(nvim): use tokyonight insteadof catppuccin

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,26 +1,12 @@
 return {
   {
-    'catppuccin/nvim',
-    name = "catppuccin",
-    config = function()
-      require('catppuccin').setup {
-        -- flavour = "latte",
-        flavour = "mocha",
-
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          treesitter = true,
-          -- fidget = true,
-          mason = true,
-          telescope = {
-            enabled = true,
-          },
-        },
-      }
-      vim.cmd.colorscheme 'catppuccin'
-    end,
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function() 
+      vim.cmd.colorscheme 'tokyonight-night'
+    end
   },
   {
     'mg979/vim-visual-multi'
