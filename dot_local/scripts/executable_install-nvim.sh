13bf40f3e546a021c21a0ca52e2c2b8f9f31c8a1
Quentin <contact@yovach.dev>
2025-06-01 18:11:47 +0200

    chore: remove executable to install nvim and remove check on git config

--- dot_local/scripts/executable_install-nvim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt install libfuse2
-
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod 755 nvim.appimage
-sudo chown root:root nvim.appimage
-sudo mv nvim.appimage /usr/local/bin/nvim
