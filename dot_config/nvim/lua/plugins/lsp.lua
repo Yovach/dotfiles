b80eeca77154b1d2f1f691dab9474e32af29e796
Quentin <contact@yovach.dev>
2025-07-17 18:46:47 +0200

    fix(nvim): remove duplicate code and update dependencies

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -52,30 +52,12 @@ return {
     end
 
     require('mason').setup()
-    require('mason-lspconfig').setup {
+    require('mason-lspconfig').setup({
       ensure_installed = servers,
-    }
+    })
 
     -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-    -- Ensure the servers above are installed
-    local mason_lspconfig = require 'mason-lspconfig'
-
-    mason_lspconfig.setup {
-      ensure_installed = servers,
-    }
-
-    mason_lspconfig.setup_handlers {
-      function(server_name)
-        require('lspconfig')[server_name].setup {
-          capabilities = capabilities,
-          on_attach = on_attach,
-          settings = servers[server_name],
-          filetypes = (servers[server_name] or {}).filetypes,
-        }
-      end,
-    }
   end,
 }
